e440ade6068940ade262c502dbda450c4b11114a
Savero Arkabuana <savero.arkabauana@gmail.com>
2022-09-22 10:28:21 +0700

    lab3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,6 +3,9 @@ from wishlist.views import show_wishlist
 from wishlist.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_json #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_json_by_id #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -11,4 +14,7 @@ urlpatterns = [
     path('xml/', show_xml, name="show_xml"),
     path('json/', show_json, name="show_json"),
     path('json/<int:id>', show_json_by_id, name="show_json_by_id"),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
